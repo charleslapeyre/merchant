60eba0dc39b323b19643f0b013a7e42349dc90e3
charleslapeyre <lapeyrecc@gmail.com>
2017-02-14 10:45:33 -0600

    setup admin namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :products
-  resources :brands
-  resources :categories
+  namespace :admin do
+    resources :products
+    resources :brands
+    resources :categories
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
