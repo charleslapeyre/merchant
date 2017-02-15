38895a292d4b964170c0e1ffdc625a39e5381836
charleslapeyre <lapeyrecc@gmail.com>
2017-02-15 08:30:43 -0600

    add styles

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,4 +7,8 @@ class Product < ApplicationRecord
                               thumb: "100x100#" },
                     default_url: "missing_:style.jpg"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
+  validates :name, presence: true
+  validates :price, presence: true
+
 end
