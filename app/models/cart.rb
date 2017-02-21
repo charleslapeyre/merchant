a28ed26efd5d178ad653c0248eaf41f9a0a84894
charleslapeyre <lapeyrecc@gmail.com>
2017-02-21 08:58:55 -0600

    added the ability to incrementally add cart items with the same product id

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -8,11 +8,12 @@ class Cart < ApplicationRecord
     current_item = line_items.find_by(product_id: product_id)
 
     if current_item
-      current_item.quantity =+ 1
+      current_item.quantity += 1
     else
       current_item = line_items.build(product: product, price: product.price)
     end
 
+    current_item
   end
 
   def subtotal
